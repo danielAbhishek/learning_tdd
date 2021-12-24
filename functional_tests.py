e924e38204aaf050e32ca68a2ac189305ca7c6fe
danielAbhishek <daniel.abhishek002@gmail.com>
2021-12-24 11:44:52 +0530

    adding additional item and checking using FT

--- functional_tests.py
+++ functional_tests.py
@@ -27,13 +27,24 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+
+        # sleep
         time.sleep(1)
 
+        # entering another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # sleep
+        time.sleep(1)
+
+        # pages updated and shows both items on the list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to fly',
+                      [row.text for row in rows])
 
         self.fail('Finish the test!')
 
